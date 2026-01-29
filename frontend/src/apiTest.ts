546137fc59a6b995725399dd01cf6c97e0d81232
MR-SAJAN-SAH <157885911+MR-SAJAN-SAH@users.noreply.github.com>
2026-01-29 23:43:38 +0530

    Remove test file

--- frontend/src/apiTest.ts
+++
@@ -1,7 +0,0 @@
-// Quick test to verify getApiUrl is working
-import { getApiUrl } from './utils/apiHelper';
-
-// Test the function
-console.log('🔍 API URL Test:');
-console.log('getApiUrl("/exams/test"):', getApiUrl('/exams/test'));
-console.log('import.meta.env.VITE_API_URL:', import.meta.env.VITE_API_URL);
